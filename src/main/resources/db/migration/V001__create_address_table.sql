02d37af5585fe23f2d549ed4359061b6afba9e1f
Violetta Korolkova <la_bella.vita@mail.ru>
2023-08-28 09:52:35 +0200

    schema

--- src/main/resources/db/migration/V001__create_address_table.sql
+++ src/main/resources/db/migration/V001__create_address_table.sql
@@ -1,14 +1,14 @@
 CREATE SEQUENCE address_table_sequence START 4 INCREMENT 1;
 
-CREATE TABLE if not exists address_table
-(
-    id          int8 primary key not null,
-    city        varchar,
-    street      varchar,
+CREATE TABLE if not exists address_table(
+    id int8 primary key not null,
+    city varchar,
+    street varchar,
     date_create timestamp
 );
 
-insert into address_table(id, city, street, date_create)
+insert into address_table(id,city,street,date_create)
 values (1, 'Moscow', 'Nevsky', null),
        (2, 'Vladimir', 'Lenina', null),
        (3, 'Kaliningrad', 'Lukashova', null);
+
--- src/main/resources/db/migration/V002__create_apartment_table.sql
+++ src/main/resources/db/migration/V002__create_apartment_table.sql
@@ -1,15 +1,14 @@
 CREATE SEQUENCE apartment_table_sequence START 4 INCREMENT 1;
 
-CREATE TABLE if not exists apartment_table
-(
-    id           int8 primary key not null,
-    room_count   int4,
-    price        int4,
+CREATE TABLE if not exists apartment_table(
+    id int8 primary key not null,
+    room_count int4,
+    price int4,
     total_rating int4,
-    address_id   int8 references address_table (id)
+    address_id int8 references address_table(id)
 );
 
-insert into apartment_table(id, room_count, price, total_rating, address_id)
+insert into apartment_table(id,room_count,price,total_rating, address_id)
 values (1, 4, 20000, null, 3),
        (2, 6, 149900, null, 2),
        (3, 1, 36700, null, 1);
